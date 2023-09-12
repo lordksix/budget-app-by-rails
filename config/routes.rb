51cc4e4059cc537e05aacaf9d3c40a7b0bcfd9bd
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-11 22:52:15 -0500

    Setup home and splash screen routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "homes#intro"
+
+  resources :homes, only: [:intro, :index], :path => 'home'
 end
