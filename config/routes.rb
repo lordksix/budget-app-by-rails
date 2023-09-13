4e4a14029b960e546054a46e941f32ea48f3d181
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-12 21:33:43 -0500

    Implement login and rsign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,6 +9,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "homes#index"
 
-  resources :homes, only: [:index], :path => 'home'
-  resources :groups, only: [:index, :show, :new, :edit]
+  resources :groups, only: [:index], :path => 'home'
+  resources :groups, only: [:show, :new, :edit]
 end
