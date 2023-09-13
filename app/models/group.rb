a88ddd5cdc75e0dec37e641af37e121509074ba8
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-13 13:15:37 -0500

    Update models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,16 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :spendings, dependent: :destroy
+  has_and_belongs_to_many :spendings
 
   validates :name, presence: { message: "can't be blank" },
                    length: { maximum: 100, message: 'is too long. Max: 100 characteres' }
+
+  before_save :update_total_spending
+
+  def update_total_spending
+    self.total_spending = spendings.reduce(0.0) do |acc, item|
+      p item.amount
+      acc + item.amount
+    end
+  end
 end
