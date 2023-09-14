3f6a17dc75f5b34a0538413e78ec85c56fc2cf4b
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-14 05:30:44 -0500

    Fix linter errors

--- spec/requests/spendings_spec.rb
+++ spec/requests/spendings_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "Spendings", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/spendings/index"
+RSpec.describe 'Spendings', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/spendings/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/spendings/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/spendings/show'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/spendings/new"
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/spendings/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/spendings/edit"
+  describe 'GET /edit' do
+    it 'returns http success' do
+      get '/spendings/edit'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
